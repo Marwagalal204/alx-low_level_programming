2bb002c9c0e18e76c9531e46170859abbcac1e0b
Marwagalal204 <Marwagalal204@gmail.com>
2023-08-10 09:50:37 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 /**
- * in this file i will print this text Programming is like building a multilingual puzzle
+ * in this file i will print tex about c
 */
 #include <stdio.h>
 /**
  * printing  string using put function
  * main is my entery point
- * this should alwys return zero for success
+ * this should alwys return 0 for success
 */
 
 int main(void)
