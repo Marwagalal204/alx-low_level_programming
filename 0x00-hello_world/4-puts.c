a3a0935e5539dc3b6f854741516ec06c1f5e9545
Marwagalal204 <Marwagalal204@gmail.com>
2023-08-10 09:42:50 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
-#include <stdio.h>
+#include <stdio>
+/**
+ * printing  string using put function
+ * main is my entery point
+*/
 
-int main (void)
+int main(void)
 {
-	 puts ("\"Programming is like building a multilingual puzzle \n");
-	 return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 }
