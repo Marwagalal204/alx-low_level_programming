07202453546131a58c8a8f45692f4d429b2958da
Marwagalal204 <Marwagalal204@gmail.com>
2023-08-10 09:34:47 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,7 @@
-int main () {
- puts ("Programming is like building a multilingual puzzle \n");
- return 0;
+#include <stdio.h>
+
+int main (void)
+{
+	 puts ("\"Programming is like building a multilingual puzzle \n");
+	 return (0);
 }
