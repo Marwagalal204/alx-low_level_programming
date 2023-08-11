ee0c68143d011f3eb310a795f286237420414cdd
Marwagalal204 <Marwagalal204@gmail.com>
2023-08-11 11:44:38 -0700

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entery point
+ * Descrption - using if and else statement
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
