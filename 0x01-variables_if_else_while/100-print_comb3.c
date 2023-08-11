ec2a311729e8abb53324218b1fd67cbde5685d9e
Marwagalal204 <Marwagalal204@gmail.com>
2023-08-11 13:47:04 -0700

    Task100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,21 @@
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - Entery point
+ * Descrption - using if and else statement
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		putchar('0' + c);
+		putchar('0');
+	}
+	putchar('\n');
+	return (0);
+}
