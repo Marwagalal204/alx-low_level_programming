b6026b8af27f202fe673b39d600dfddd1f47d4b6
Marwagalal204 <Marwagalal204@gmail.com>
2023-08-11 12:54:58 -0700

    Task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entery point
+ * Descrption - using if and else statement
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		printf("%d,", c);
+	}
+	printf("\n");
+	return (0);
+}
