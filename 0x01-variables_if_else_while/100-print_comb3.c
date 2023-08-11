d968e62e15d794b4ac0d3dbd644cabc9fc4ffa75
Marwagalal204 <Marwagalal204@gmail.com>
2023-08-11 13:58:20 -0700

    task100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,7 +14,15 @@ int main(void)
 	for (c = 0; c <= 9; c++)
 	{
 		putchar('0' + c);
+		putchar(',');
+		putchar(' ');
 		putchar('0');
+		if (c != 9)
+		{
+		putchar('0' + c);
+		putchar(',');
+		putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
