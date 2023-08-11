405d6924a8c252c096cc344a3e8a6193970bd50c
Marwagalal204 <Marwagalal204@gmail.com>
2023-08-11 13:17:40 -0700

    Task9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,10 +12,11 @@ int main(void)
 
 	for (c = 0; c <= 9; c++)
 	{
-		putchar ('0' + c);
+		putchar('0' + c);
 		if (c != 9)
 			putchar(',');
+		putchar('0');
 	}
-	printf("\n");
+	putchar('\n');
 	return (0);
 }
