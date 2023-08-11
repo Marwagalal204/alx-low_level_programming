be87b92930eecbbf0d8e605c578daec1cdbbd67e
Marwagalal204 <Marwagalal204@gmail.com>
2023-08-11 12:29:19 -0700

    Task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - Entery point
+ * Descrption - using if and else statement
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c <= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
