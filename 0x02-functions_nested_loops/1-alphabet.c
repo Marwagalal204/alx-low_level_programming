69d032773538d5d5edcf1fb0800c0fcfbf3a1502
marwagalal204 <marwagalal204@gmail.com>
2023-08-15 09:27:53 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - Entery point
+ * Descrption - using if and else statement
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,10 @@
 int _putchar(char c);
+void print_alphabet(void)
+{
+	char i;
+	for( i = 'a' ; i <= 'z' ; i++)
+	{
+		 _putchar(i);
+	}
+       	_putchar('\n');
+}
