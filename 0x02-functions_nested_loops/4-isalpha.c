977938ff4bf345777769c9acf478241dbc427607
marwagalal204 <marwagalal204@gmail.com>
2023-08-15 10:44:05 -0700

    task4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * _islower - a function to check character
- * @c : The character to be checked
+ * @c : The recieve character
  * Return: 1 if true. 0 if false.
  */
 int _isalpha(int c)
 {
-	if ((c >= 'A' && c <= 'Z' ) ||  (c >= 'a' && c <= 'z'))
+	if ((c >= 'A' && c <= 'Z') ||  (c >= 'a' && c <= 'z'))
 	{
 		return (1);
 	}
