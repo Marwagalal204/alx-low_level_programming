f19566c5f0c257a28d6357665e52b8055cbd363f
marwagalal204 <marwagalal204@gmail.com>
2023-08-15 07:38:36 -0700

    Task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entery point
+ * Descrption - print with printf function
+ * Return: Always 0 (Success)
+*/
+int main()
+{
+	_putchar("_putchar");
+	return(0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+in this directory i will learn more about c functions
