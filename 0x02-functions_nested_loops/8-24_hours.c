98ac30eb826a4cd15eade632a503311f704a28cf
Marwagalal204 <Marwagalal204@gmail.com>
2023-08-16 20:53:03 +0300

    task8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,11 +11,11 @@ void jack_bauer(void)
 	for (hour = 0; hour <= 23; hour++)
 	for (min = 0; min <= 59; min++)
 	{
-		_putchar(hour);
-		_putchar(hour);
+		_putchar((hour / 10) + '0');
+		_putchar((hour % 10) + '0');
 		_putchar(':');
-		_putchar(min);
-		_putchar(min);
+		_putchar((min / 10) + '0');
+		_putchar((min % 10) + '0');
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
