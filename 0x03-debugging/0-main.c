57e8a61b492013c7205c000d0a20e01584e8a2de
Marwagalal204 <Marwagalal204@gmail.com>
2023-08-17 00:22:58 +0300

    task0-debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,11 +3,12 @@
 /**
  * * main - tests function that prints if integer is positive or negative
  * * Return: 0
- * */
+**/
 
 int main(void)
 {
 	int n;
+
 	n = 0;
 	positive_or_negative(n);
 	return (0);
