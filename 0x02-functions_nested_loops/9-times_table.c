00e8d4da2ec51643167bcff003a693461dc144ce
Marwagalal204 <Marwagalal204@gmail.com>
2023-08-16 21:46:33 +0300

    Task9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,26 +6,11 @@
 
 void times_table(void)
 {
-int num, mult, prod;
+	int i, m;
 
-for (num = 0; num <= 9; num++)
-{
-_putchar('0');
-
-for (mult = 1; mult <= 9; mult++)
-{
-_putchar(',');
-_putchar(' ');
-
-prod = num * mult;
-
-if (prod <= 9)
-_putchar(' ');
-else
-_putchar((prod / 10) + '0');
-
-_putchar((prod % 10) + '0');
-}
-_putchar('\n');
-}
+	for (i = 0; i <= 9; i++)
+		m = (i * 9);
+	{
+		_putchar(m + '0');
+	}
 }
