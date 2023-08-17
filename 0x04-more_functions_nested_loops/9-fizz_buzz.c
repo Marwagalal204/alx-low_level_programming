a44375ca10363bd5a8d90aafdfc46d004c5df63d
Marwagalal204 <Marwagalal204@gmail.com>
2023-08-18 00:01:16 +0300

    tsk9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,17 +12,17 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
-			printf(" Fizz");
+			printf("Fizz ");
 		else if (i % 5 == 0 && i % 3 != 0)
-			printf(" Buzz");
+			printf("Buzz ");
 		else if (i % 3 == 0 && i % 5 == 0)
-			printf(" FizzBuzz");
+			printf("FizzBuzz ");
 		else if (i == 1)
 			printf("%d", i);
 		else if (i == 100)
-			printf(" %d", i);
+			printf("Buzz");
 		else
-			printf(" %d ", i);
+			printf("%d ", i);
 	}
 	printf("\n");
 	return (0);
