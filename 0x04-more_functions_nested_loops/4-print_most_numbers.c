9edc42055e8ca03f4de37c2d7c47831c3716d0b8
Marwagalal204 <Marwagalal204@gmail.com>
2023-08-17 12:00:39 +0300

    Task4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers - Entery point
+ *
+ * @i: integer
+*/
+
+void print_most_numbers(void);
+{
+	int i;
+
+	for (i = ; i <= 9; i++)
+	{
+		if (i != 4 && i != 2)
+		_putchar(i + '0')
+	}
+}		
