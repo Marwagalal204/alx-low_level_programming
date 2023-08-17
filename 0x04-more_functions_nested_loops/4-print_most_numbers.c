5a54fb3c464e2d0f77d1ca861fcb021d34f51792
Marwagalal204 <Marwagalal204@gmail.com>
2023-08-17 13:39:00 +0300

    tsk4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,13 +6,13 @@
  * Return: from 0 to 9 except 2 nd 4
 */
 
-void print_most_numbers(void);
+void print_most_numbers(void)
 {
 	int i;
 
 	for (i = 0; i <= 9; i++)
 	{
 		if (i != 4 && i != 2)
-		_putchar(i + '0')
+		_putchar(i + '0');
 	}
 }
