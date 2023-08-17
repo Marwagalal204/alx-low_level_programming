eae13e80e96524313704da48543c050248af656e
Marwagalal204 <Marwagalal204@gmail.com>
2023-08-17 15:06:44 +0300

    Task5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,6 +2,24 @@
 
 void more_numbers(void)
 {
-	int i;
-	for (i = ; i <= 14; i++)
+	int i, n = 0;
 
+	while (n < 10)
+	{
+
+		for (i = 0; i <= 14; i++)
+		
+			if (i <= 9)
+		{
+			 _putchar(i + '0');
+		}
+			else
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+	_putchar('\n');
+	n++;
+		
+	}
+}
