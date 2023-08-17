d57fd317de62310daee44349bea549427d237781
Marwagalal204 <Marwagalal204@gmail.com>
2023-08-17 18:20:05 +0300

    task9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
+/**
+ * main - take the fizz-buzz challenge
+ *
+ * Return: Always 0
+*/
 
 int main(void)
 {
-	int i = 1;
-	int t = (i * 3);
-	int f = (i * 5);
-	int tf = (i * 15);
-	
+	int i;
+
 	for (i = 1; i <= 100; i++)
 	{
-		if (i == t)
+		if ((i % 3 == 0) && (i % 15 != 0))
 			printf("Fizz ");
-		else if (i == f)
+		else if ((i % 5 == 0) && (i % 15))
 			printf("Buzz ");
-		else if (i == tf)
+		else if (i % 15 == 0)
 			printf("FizzBuzz ");
 		else
 			printf("%d ", i);
 	}
 	printf("\n");
-	i++;
 	return (0);
 }
