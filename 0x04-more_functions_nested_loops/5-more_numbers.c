3ff08da602f653db90f585ff997b48c4c22f302c
Marwagalal204 <Marwagalal204@gmail.com>
2023-08-17 16:51:15 +0300

    task5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,30 +1,25 @@
 #include "main.h"
 /**
- *  more_numbers - prints 0 - 14
- *
- *   Return: void
-*/
+ *  *  more_numbers - prints 0 - 14
+ *   *
+ *    *   Return: void
+ *    */
 
 void more_numbers(void)
 {
 	int i, n = 0;
-
 	while (n < 10)
 	{
-
 		for (i = 0; i <= 14; i++)
-		
-			if (i <= 9)
-		{
-			 _putchar(i + '0');
-		}
-			else
+	{
+		if (i  > 9)
 		{
 			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
 		}
-	_putchar('\n');
-	n++;
-		
+		 _putchar((i % 10) + '0');
+	}
+		_putchar('\n');
+		n++;
 	}
 }
+
