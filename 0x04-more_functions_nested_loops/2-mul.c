72408be5b7c669b57d4cc49f9270c9eeb9962385
Marwagalal204 <Marwagalal204@gmail.com>
2023-08-17 11:30:12 +0300

    Task2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - Entery point
+ * @a:firdt int
+ * @b: second int
+ * Return: 0 for succeess
+ */
+
+int mul(int a, int b)
+{
+	int sum = (a * b);
+
+	_putchar(sum);
+	return (0);
+}
