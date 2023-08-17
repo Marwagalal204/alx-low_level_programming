b172259785e93eddd830ac3107dd5c5e52191d92
Marwagalal204 <Marwagalal204@gmail.com>
2023-08-17 12:27:44 +0300

    task0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,15 +8,11 @@
 
 int _isupper(int c)
 {
-
-	char i;
-
-	for (i = 65; i <= 90; i++)
 	{
-		if (c == i)
-			return (1);
-		else
-			return (0);
+	if (c >= 65 && c <= 89)
+		return (1);
+	else
+		return (0);
 	}
 	return (0);
 }
