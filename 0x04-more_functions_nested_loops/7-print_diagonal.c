08f59ed9dd515713d7646bc2bc3954e6e8613847
Marwagalal204 <Marwagalal204@gmail.com>
2023-08-17 23:58:37 +0300

    tsk7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,12 +8,17 @@
 
 void print_diagonal(int n)
 {
-	int i;
+	int i, a;
 
 	for (i = 0; i < n; i++)
 	{
 		if (n > 0)
+		{
+			for (a = 0; a < i; a++)
+				_putchar(' ');
 			_putchar('\\');
+			_putchar('\n');
+		}
 		else
 			_putchar('\n');
 	}
