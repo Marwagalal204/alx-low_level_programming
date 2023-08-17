543c3f20ed1ddcf8c50a47eb2b6648ec7800e81f
Marwagalal204 <Marwagalal204@gmail.com>
2023-08-17 13:24:03 +0300

    task~

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
  * print_numbers - Entery point
- * @i: int
- * Return: 0 for succeess
+ *
+ * Return: from 0 to 9
  */
 
-void print_numbers(void);
+void print_numbers(void)
 {
 	int i;
 
 	for (i = 0 ; i <= 9; i++)
 		_putchar(i + '0');
-		_putchar(\n);
+	_putchar('\n');
 }
