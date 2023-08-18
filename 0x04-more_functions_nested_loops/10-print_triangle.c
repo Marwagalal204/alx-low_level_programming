f658bf3f1c99d0c316f55874734b410c9a84ad6d
Marwagalal204 <Marwagalal204@gmail.com>
2023-08-18 23:47:29 +0300

    task10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,27 +1,26 @@
 #include "main.h"
 
 /**
- * print_diagonal - prints a line of \ n long
- *  @n: length of line
- *   Return: void
+ * print_triangle - prints a # of \ n long
+ * @size: length of line
+ * Return: void
 */
 
 void print_triangle(int size)
 {
-	int space, hash;
+	int space, hash, space1;
 
-	for (space = size; space < size; space++)
+	if (size <= 0)
+		_putchar('\n');
 
+	for (space = size - 1; space > -1; space--)
 
 	{
-		for (hash = 0; hash < space; hash++)
-		_putchar(' ');
-		_putchar('#');
+		for (hash = 0; hash < space ; hash++)
+			_putchar(' ');
+
+		for (space1 = space; space1 < size; space1++)
+			_putchar('#');
 		_putchar('\n');
-		_putchar('#');	
-		{
-			if (size <= 0)
-			_putchar('\n');
-		}
 	}
 }
