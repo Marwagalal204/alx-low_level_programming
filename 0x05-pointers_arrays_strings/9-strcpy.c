cc07f18399ac75efcf1e645f896faa4ad1b6d63c
Marwagalal204 <Marwagalal204@gmail.com>
2023-08-21 18:24:54 +0300

    task9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,11 +10,11 @@
 char *_strcpy(char *dest, char *src)
 {
 	int n, i = 0;
-	
-	for (n = 0; *src != '\0'; n++)
+
+	for (n = 0; *src != '\0'; ++n)
 	{
 		dest[i] = src[n];
-		i++;
+		++i;
 	}
 	dest[i] = '\0';
 	return (dest);
