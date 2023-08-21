f2cf99be71e42e4e169cb820d808d841c6ddd421
Marwagalal204 <Marwagalal204@gmail.com>
2023-08-21 18:12:44 +0300

    task7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts_hlf - print half string
+ * @str: inputs
+ * descrption: function that print half string
+*/
+
+void puts_half(char *str)
+{
+	int i, count = 0;
+
+	while (*str != '\0')
+	{
+		str++;
+		count++;
+	}
+	str -= count;
+	for (i = count / 2; i < count; i++)
+	{
+		_putchar(*str + i);
+	}
+	_putchar('\n');
+}
+
+
