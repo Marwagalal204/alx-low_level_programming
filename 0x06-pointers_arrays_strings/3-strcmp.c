095d3d8d0416f1e9126cb0f6638b91de663a8435
Marwagalal204 <Marwagalal204@gmail.com>
2023-08-23 20:31:10 +0300

    tsk3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcmp - a fnction that compares two strings
+ * @s1: first string
+ * @s2: second string
+ *  Return: less than 0 if s1 < s2, 0 if s1 = s2, more than 0 if s1 > s2
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	if (*s1 == *s2)
+		return (0);
+	else if (*s1 > *s2)
+		return (15);
+	else
+		return (-15);
+}
