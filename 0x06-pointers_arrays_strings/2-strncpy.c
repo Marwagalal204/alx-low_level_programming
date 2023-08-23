a503f82fa5ab41a82a23458a8ae20a4d30f7d924
Marwagalal204 <Marwagalal204@gmail.com>
2023-08-23 19:59:10 +0300

    task2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,2 +1,23 @@
+#include "main.h"
+/**
+ * _strncpy - fuction copy two strings with n value
+ * @dest: sting destanation
+ * @src: string to be concatanating
+ * @n: length of src
+ * Descrption: create  fucnction that cobies two strings
+ * Return: dest
+*/
 
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
 
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+
+}
