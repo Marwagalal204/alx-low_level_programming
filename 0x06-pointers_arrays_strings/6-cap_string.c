300b8edd3f60661244788ae823a5d44b71be7049
Marwagalal204 <Marwagalal204@gmail.com>
2023-08-24 01:07:46 +0300

    tsk6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,15 +1,21 @@
 #include "main.h"
+/**
+ * cap_string - print string nd make first letter capital
+ * @str: points to start of array
+ * Descrption: print string and make first letter capital
+ * return: resulting string
+*/
 
 char *cap_string(char *str)
 {
 	while (*str != '\0')
 	{
-		if (*str == 32 || *str == 46 || *str == 9 || *str == '\n') 
+		if (*str == 32 || *str == 46 || *str == 9 || *str == '\n')
 		{
 			str++;
 			if (*str >= 97 && *str <= 122)
 				*str = *str - 32;
-		} 
+		}
 		str++;
 	}
 	return (str);
