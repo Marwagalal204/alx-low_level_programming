03b788660a6015405563973dc09cf88b88476b90
Marwagalal204 <Marwagalal204@gmail.com>
2023-08-28 13:50:40 +0300

    tsk3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,15 +8,15 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j, count = 0;
+	unsigned int i, j;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (s[i] == accept[j])
-				count++;
+			if (s[i] == accept[j] && accept[j] == '\0')
+				break;
 		}
 	}
-	return (count);
+	return (i);
 }
