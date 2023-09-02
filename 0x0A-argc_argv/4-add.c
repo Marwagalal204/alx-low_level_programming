c186973a5162d9334cdf244b228e3ad624637372
Marwagalal204 <Marwagalal204@gmail.com>
2023-09-02 17:41:56 +0300

    task4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,23 +12,9 @@
 
 int main(int argc, char *argv[])
 {
-	int i, n;
-
 	if (argc < 1)
 		printf("0\n");
 	return (0);
-
-	for (i = 0; i < argc; i++)
-	{
-		for (n = 0; argv[i][n] != '\0'; n++)
-		{
-			if (!(isdigit(argv[i][n])))
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-	}
 	printf("%d\n", atoi(argv[1]) + atoi(argv[2]));
 	return (0);
 }
