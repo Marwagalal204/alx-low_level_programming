de52e36c5c55a4461992309bae291bb3101d8bef
Marwagalal204 <Marwagalal204@gmail.com>
2023-09-02 17:37:15 +0300

    task4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,46 +1,7 @@
 #include <stdio.h>
-
-/**
- * _isdigit - Entery point
- * @c:cheks the digits
- * Return: 1 if true. 0 if false
-*/
-
-int _isdigit(int c)
-{
-	if (c >= '0' && c <= '9')
-		return (1);
-	else
-		return (0);
-}
-
-/**
- * atoi - convert string to integer.
- * @s: string to be converted
- * Return: integer converted from the string
-*/
-
-int atoi(char *s)
-{
-	int sign = 1;
-
-	unsigned int num = 0;
-
-	do {
-		if (*s == '-')
-			sign *= -1;
-
-		else if (*s >= '0' && *s <= '9')
-			num = (num * 10) + (*s - '0');
-
-		else if (num > 0)
-			break;
-	}
-		while (*s++);
-	{
-		return (num * sign);
-	}
-}
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 /**
  * main - adds positive numbers.
@@ -51,20 +12,23 @@ int atoi(char *s)
 
 int main(int argc, char *argv[])
 {
-	int i, n, sum = 0;
+	int i, n;
 
-	for (i = 1; i < argc; i++)
+	if (argc < 1)
+		printf("0\n");
+	return (0);
+
+	for (i = 0; i < argc; i++)
 	{
 		for (n = 0; argv[i][n] != '\0'; n++)
 		{
-			if (!_isdigit(argv[i][n]))
+			if (!(isdigit(argv[i][n])))
 			{
 				printf("Error\n");
 				return (1);
 			}
 		}
-		sum = sum + atoi(argv[i]);
 	}
-	printf("%d\n", sum);
+	printf("%d\n", atoi(argv[1]) + atoi(argv[2]));
 	return (0);
 }
