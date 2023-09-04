6c4e6f3e3f16f243c6bc4dfc84185517ba23d115
Marwagalal204 <Marwagalal204@gmail.com>
2023-09-04 20:08:23 +0300

    task4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * free_grid - function frees 2d array memory
- * @grid: matrix to be freed
- * @hieght: hieght of matrex
+ * free_grid - function frees 2d array
+ * @grid: 2d array to be freed
+ * @height: hieght of 2d array
 */
 void free_grid(int **grid, int height)
 {
