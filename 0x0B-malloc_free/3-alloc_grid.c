7f292796ef2ed30bd8d94e8fac5e141d3b601faa
Marwagalal204 <Marwagalal204@gmail.com>
2023-09-04 19:39:54 +0300

    tsk3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - function retun pointer to a 2 dimensional.
+ * @width: coulmn of array
+ * @height: rows of array
+ * Return: Null for failure and 2d array for success.
+*/
+
+int **alloc_grid(int width, int height)
+{
+	int **arr, i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	arr = (int **)malloc(height * sizeof(int));
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = malloc(width * sizeof(int));
+		if (arr[i] == NULL)
+		{
+			free(arr);
+			for (j = 0; j < height; j++)
+			free(arr[j]);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+		{
+			arr[i][j] = 0;
+		}
+	}
+
+	return (arr);
+}
+
