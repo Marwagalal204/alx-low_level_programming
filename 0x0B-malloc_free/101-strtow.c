d7885b8ba9dc05897045430f5db8352dfe90623d
Marwagalal204 <Marwagalal204@gmail.com>
2023-09-06 17:08:15 +0300

    tsk101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -3,6 +3,31 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stddef.h>
+
+/**
+ * wordlen - function counts string long
+ * @s: inputed string
+ * Return: ord count.
+*/
+int wordlen(char *s)
+{
+	int i, flag, words = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n')
+		{
+			flag = 0;
+		}
+		else if (flag != 0)
+		{
+			words++;
+			flag = 1;
+		}
+	}
+	return (words);
+}
+
 /**
  * strtow - a function that splits a string into words.
  * @str: input string
@@ -11,33 +36,41 @@
 
 char **strtow(char *str)
 {
-	int i, flag, words = 0;
-	char **arr, *arr1;
+	int l = 0, w, n, i, ch, last, begin;
+	char **words, *wrd;
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (*(str + l))
+		l++;
+
+	w = wordlen(str);
+
+	words = (char **) malloc(sizeof(char *) * (w + 1));
+
+	if (words == NULL)
+		return (NULL);
+
+	for (i = 0; i <= l; i++)
 	{
+		if (str[i] == ' ' || str[i] == '\0')
+		{
+			if (ch)
+			{
+				last = i;
+				wrd = (char *) malloc(sizeof(char) * (ch + 1));
 
-		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
-		{
-			flag = 0;
-		}
-		else
-		{
-			*arr1 = str[i];
-			arr1++;
-		}
+					if (wrd == NULL)
+						return (NULL);
 
-		if (flag != 0)
-		{
-			**arr = *arr1;
-			arr++;
-			words++;
-			arr1++;
-			*arr1 = '\0';
-			arr[words] = NULL;
-			words++;
-			flag = 1;
+				while (begin < last)
+					*wrd++ = str[begin++];
+				*wrd = '\0';
+				words[n] = wrd - ch;
+				n++;
+				ch = 0;
+			}
 		}
+		else if (ch == 0)
+			begin = i;
 	}
-	return (arr);
+	return (words);
 }
