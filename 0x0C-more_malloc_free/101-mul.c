efb9cdbb625c6e6286174249438ad5b835e696d1
Marwagalal204 <Marwagalal204@gmail.com>
2023-09-07 18:00:45 +0300

    task101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,44 +3,6 @@
 #include <stdlib.h>
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- * Return: the int converted from the string
-*/
-int _atoi(char *s)
-{
-	int i, d, n, len, f, digit;
-
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
-	{
-		if (s[i] == '-')
-			++d;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-		}
-		i++;
-	}
-	return (n);
-}
-
-/**
  * main - Entery point
  * @argc: number og agumentd
  * @argv: array of argument
@@ -55,6 +17,6 @@ int main(int argc, char **argv)
 		exit(98);
 	}
 	else
-		printf("%d\n", _atoi(argv[1]) * atoi(argv[2]));
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
