0f52c0b3975557470d6936f6989c98fe37727b40
Marwagalal204 <Marwagalal204@gmail.com>
2023-09-08 02:52:43 +0300

    task101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -11,12 +11,21 @@
 
 int main(int argc, char **argv)
 {
+	int m;
+	char *p;
+
 	if (argc < 3)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+
 	else
-		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		m = atoi(argv[1]) * atoi(argv[2]);
+	p = malloc(m);
+	if (p == NULL)
+		return (0);
+	else
+	printf("%d", m);
 	return (0);
 }
