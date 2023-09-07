78b440a98fb600ff4477dc4e87f00eadb460ff3d
Marwagalal204 <Marwagalal204@gmail.com>
2023-09-07 16:49:55 +0300

    task100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,7 +13,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i;
 	char *ch, *pt = ptr;
 
-	if (new_size == 0 || ptr != NULL)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
@@ -26,21 +26,17 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	if (new_size == old_size)
 		return (ptr);
-	else if (new_size == 0)
-	{
-		free(ptr);
-		return (NULL);
-	}
 
 	ch = malloc(new_size);
-
 	if (ch == NULL)
 		return (NULL);
+
 	if (new_size > old_size)
 		new_size = old_size;
 
 	for (i = 0; i < new_size; i++)
 		ch[i] = pt[i];
+
 	free(ptr);
 	return (ch);
 }
