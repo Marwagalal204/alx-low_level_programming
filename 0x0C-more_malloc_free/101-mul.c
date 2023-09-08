9f31bedcfaa8407236d8704c92fa2907230d5822
Marwagalal204 <Marwagalal204@gmail.com>
2023-09-08 23:11:04 +0300

    tsk5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -13,10 +13,19 @@ int main(int argc, char **argv)
 {
 	if (argc < 3)
 	{
+		                printf("Error\n");
+				                exit(98);
+						        }
+	if (*argv[1] < 48 && *argv[1] > 57)
+	{
+		                printf("Error\n");
+				                exit(98);
+						        }
+	if (*argv[2] < 48 && *argv[2] > 57)
+	{
 		printf("Error\n");
 		exit(98);
 	}
-	else
-		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
