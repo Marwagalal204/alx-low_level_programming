45d2829b38b7af9b85d43f53ef783cbb23a360c0
Marwagalal204 <Marwagalal204@gmail.com>
2023-09-08 21:22:12 +0300

    task101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -10,39 +10,47 @@
 
 char **strtow(char *str)
 {
-	int i, n, j, word = 0, c = 0, len = 0;
-	char **ptr;
+	        int i, n, j, word = 0, c = 0, len = 0;
+		        char **ptr;
 
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (str[i] == ' ' || str[i] == '\t')
-			word++;
-	}
-	if (word == 0)
-		return (NULL);
+			        for (i = 0; str[i] != '\0'; i++)
+					        {
+							                if (str[i] == ' ' || str[i] == '\t')
+										                        word++;
+									        }
+				        word++;
+					        
+					        printf("%d", word);
+						        if (word == 0)
+								                return (NULL);
 
-	ptr = malloc(word + 1);
-	if (ptr == NULL)
-		return (NULL);
+							        ptr = malloc(word + 1);
+								        if (ptr == NULL)
+										                return (NULL);
 
-	for (n = 0; n < word + 1; n++)
-	{
-		for (j = len; str[j] != ' ' && str[j] != '\t'; j++)
-			;
-		ptr[n] = malloc(j - len + 1);
-		if (ptr == NULL)
-			return (NULL);
-		for (c = len; c  < j; c++)
-		{
-			if (str[c] == ' ' || str[c] == '\t')
-			{
-				break;
-			}
-			else
-			{
-				ptr[n][c-len] = str[c];
-			}
-		}
-	}
-	return (ptr);
+									        for (n = 0; n < word; n++)
+											        {
+													                for (j = len; str[j] != ' ' && str[j] != '\t'; j++)
+																                        ;
+															                ptr[n] = malloc(j - len + 1);
+																	                if (ptr == NULL)
+																				                        return (NULL);
+																			                for (c = len; c  < j; c++)
+																						                {
+																									                        if (str[c] == ' ' || str[c] == '\t' || str[c] == '\0')
+																													                        {
+																																	                                break;
+																																					                        }
+																												                        else
+																																                        {
+																																				                         
+																																				                            printf("%d",n);
+																																							                                    ptr[n][c-len] = str[c];
+																																											                            }
+																															                }
+																					                ptr[n][c-len+1] = '\0';
+																							                j++;
+																									                len = j;
+																											        }
+										        return (ptr);
 }
