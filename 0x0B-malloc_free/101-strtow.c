29034e8b4b32b7049f58903a9137c3a41c53cb14
Marwagalal204 <Marwagalal204@gmail.com>
2023-09-09 00:10:51 +0300

    task101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,13 +1,10 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-
 /**
  * strtow - a function that splits a string into words.
  * @str: input string
  * Return: Null for failure and splites string for sucess
 */
-
 char **strtow(char *str)
 {
 	int i, n, j, word = 0, c = 0, len = 0;
@@ -15,10 +12,11 @@ char **strtow(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if ((str[i] == ' ' || str[i] == '\t') && str[i+1] != ' ' && str[i + 1] != '\t' && str[i + 1] != '\0')
+		if ((str[i] == ' ' || str[i] == '\t') && str[i + 1] != ' ')
 			word++;
 	}
-	if(str[0] != '\t' && str[0] != ' ' ) word++;
+	if (str[0] != '\t' && str[0] != ' ')
+		word++;
 	if (word == 0)
 		return (NULL);
 
@@ -31,22 +29,17 @@ char **strtow(char *str)
 		for (j = len; str[j] != ' ' && str[j] != '\t'; j++)
 			;
 		ptr[n] = malloc(j - len + 1);
-																	                if (ptr == NULL)
-																				                        return (NULL);
-																			                for (c = len; c  < j; c++)
-																						                {
-																									                        if (str[c] == ' ' || str[c] == '\t' || str[c] == '\0')
-																													                        {
-																																	                                break;
-																																					                        }
-																												                        else
-																																                        {
-																																				                         
-																																				                           																						                                    ptr[n][c-len] = str[c];
-																																											                            }
-																															                }
-																					                ptr[n][c-len+1] = '\0';
-																							                j++;
-																									                len = j;												}
-										return (ptr);
+		if (ptr == NULL)
+			return (NULL);
+		for (c = len; c  < j; c++)
+		{
+			if (str[c] == ' ' || str[c] == '\t' || str[c] == '\0')
+				break;
+			ptr[n][c - len] = str[c];
+		}
+		ptr[n][c - len + 1] = '\0';
+		j++;
+		len = j;
+	}
+	return (ptr);
 }
