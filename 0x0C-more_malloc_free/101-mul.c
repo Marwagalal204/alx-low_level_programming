5073330b412685d448e52579fa2a67650736e31c
Marwagalal204 <Marwagalal204@gmail.com>
2023-09-08 23:53:22 +0300

    tsk5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,34 +1,36 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
-
-
-
-
-
+/**
+ * main - Entery point
+ * @argc: argument count
+ * @argv: argument value
+ * Return: Exit 98 for failure mul for success
+*/
 int main(int argc, char *argv[])
 {
 	unsigned long mul;
 	int j;
+
 	if (argc != 3)
-	{ 
+	{
 		printf("Error\n");
-		exit(98); 
+		exit(98);
 	}
 		for (j = 0; argv[1][j] != '\0'; j++)
 		{
 			if (argv[1][j] > 57 || argv[1][j] < 48)
-			{  
-				printf("Error\n");
-				exit(98); 
-			}
-			else if(argv[2][j] > 57 || argv[2][j] < 48)
 			{
 				printf("Error\n");
 				exit(98);
 			}
-			else {
+			else if (argv[2][j] > 57 || argv[2][j] < 48)
+			{
+				printf("Error\n");
+				exit(98);
+			}
+			else
+			{
 			mul += argv[1][j] * argv[2][j];
 			}
 		}
