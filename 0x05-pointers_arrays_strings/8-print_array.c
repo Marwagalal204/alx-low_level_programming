1f204c4063088fdd2edb25ec929946e763b0b29d
Marwagalal204 <Marwagalal204@gmail.com>
2023-09-10 14:48:02 +0300

    task8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,6 +9,13 @@
 
 void print_array(int *a, int n)
 {
-	for (n = 0; n < *a; n++)
-		printf("%d\n", *a);
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != n - 1)
+			printf("%i, ", a[i]);
+		else
+			printf("%i", a[i]);
+	}
 }
