21cdb49f0d2a6901c3f2c4522c67fc411ec04e03
Marwagalal204 <Marwagalal204@gmail.com>
2023-09-11 16:55:05 +0300

    task5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - free struct
+ * @d: pointer to dog struct
+ * Return: void
+*/
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
