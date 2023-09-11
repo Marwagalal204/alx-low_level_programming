ce4d1c4fc2d82836db08aeecb7022cab70e23d17
Marwagalal204 <Marwagalal204@gmail.com>
2023-09-11 14:21:59 +0300

    task0

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+in this directory i will practice more about structure typedef in c programing language
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOC_H
+/*
+ * dog - a new struct with dog info
+ * @name: dog name
+ * @age: dog age
+ * @oner: dog oner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
