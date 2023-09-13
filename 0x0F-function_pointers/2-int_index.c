0fddd4638b324fcbe74839a0ad58a238a12d20b2
Marwagalal204 <Marwagalal204@gmail.com>
2023-09-13 12:40:48 +0300

    task3

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,7 +13,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (array != NULL && cmp != NULL && size > 0)
 	{
 		for (i = 0; i < size; i++)
-			return (cmp(array[i]));
+		{
+			if (cmp(array[i]))
+			return (i);
+		}
 	}
 	return (-1);
 }
