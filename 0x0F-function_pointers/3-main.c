b85b93418475a7398c4cc546f5d7a5b2788aabc3
Marwagalal204 <Marwagalal204@gmail.com>
2023-09-13 15:51:51 +0300

    task3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,20 +10,20 @@
 */
 int main(int argc, char *argv[])
 {
-	int (*ops)(int, int);
+	int (*op)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	ops = get_op_func(argv[2]);
+	op = get_op_func(argv[2]);
 
-	if (ops == NULL)
+	if (op == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	printf("%d\n", ops(atoi(argv[1]), atoi(argv[3])));
+	printf("%d\n", op(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
