ed707e644bd8177f35c671a85836e1fdccd5cd2e
Marwagalal204 <Marwagalal204@gmail.com>
2023-09-14 20:25:17 +0300

    task2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,10 +24,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		}
 		else
 		{
-			printf("nil");
+			printf("(nil)");
 		}
 
-		if (separator != NULL && i < n - 1)
+		if (separator != NULL && i < n -1)
 			printf("%s", separator);
 	}
 
