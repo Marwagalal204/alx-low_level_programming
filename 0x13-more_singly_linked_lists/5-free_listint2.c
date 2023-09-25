33d5feee7b81517c68cf97d7cd9bd03967b71d7a
marwagalal204 <marwagalal204@gmail.com>
2023-09-26 00:09:17 +0300

    task5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - a function that frees a list and set head to NULL
+ * @head: a pointer to the head list
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	if (head == NULL)
+		return;
+
+	if (*head != NULL)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+	}
+
+	*head = NULL;
+}
