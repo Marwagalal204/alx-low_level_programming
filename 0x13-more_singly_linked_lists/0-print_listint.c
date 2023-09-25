31f872b200de37f0f76dae8d4d188a6085145f56
marwagalal204 <marwagalal204@gmail.com>
2023-09-26 00:44:48 +0300

    task0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,6 +11,9 @@ size_t print_listint(const listint_t *h)
 	listint_t *tmp = (listint_t *)h;
 	size_t count = 0;
 
+	if (h == NULL)
+		return (0);
+
 	while (tmp != NULL)
 	{
 		printf("%d\n", tmp->n);
