a647c61c30153a88766cce88f11584a9bb4c3564
marwagalal204 <marwagalal204@gmail.com>
2023-09-26 13:43:35 +0300

    task6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -3,21 +3,21 @@
 /**
  * pop_listint - function that deletes the head node of a list
  * @head: head of the list
- * Return n for success and 0 for failure
+ * Return: n for success and 0 for failure
 */
 int pop_listint(listint_t **head)
 {
 	listint_t *tmp;
+	int data;
 
 	if (head == NULL || *head == NULL)
 		return (0);
 
-	tmp = (*head)->next;
-	free(*head);
-	*head = tmp;
+	tmp = *head;
+	*head = (*head)->next;
+	data = tmp->n;
+	free(tmp);
+	tmp = NULL;
 
-	if (tmp != NULL)
-		return (tmp->n);
-	else
-		return (0);
+	return (data);
 }
