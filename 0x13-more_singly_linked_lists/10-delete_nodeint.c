50e6aec76fe11e6fca16ddc59116b695186f0113
marwagalal204 <marwagalal204@gmail.com>
2023-09-27 01:40:10 +0300

    task10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -7,7 +7,7 @@
 */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *tmp;
+	listint_t *tmp, *prev = NULL;
 	unsigned int count = 0;
 
 	if (head == NULL || *head == NULL)
@@ -19,16 +19,15 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		*head = (*head)->next;
 		free(tmp);
-		tmp = NULL;
 		return (1);
 	}
 
 	while (tmp != NULL)
 	{
-		if (index == count)
+		if (count == index)
 		{
+			prev->next = tmp->next;
 			free(tmp);
-			tmp = tmp->next;
 			count++;
 		}
 		return (1);
