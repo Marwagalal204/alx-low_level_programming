b6760373990cb545e939330773a605e33d6a8918
marwagalal204 <marwagalal204@gmail.com>
2023-09-26 13:45:08 +0300

    task6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -8,16 +8,14 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *tmp;
-	int data;
 
 	if (head == NULL || *head == NULL)
 		return (0);
 
 	tmp = *head;
 	*head = (*head)->next;
-	data = tmp->n;
 	free(tmp);
 	tmp = NULL;
 
-	return (data);
+	return (tmp->n);
 }
