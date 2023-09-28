b50bc421359a56fc3c012ac24fe764a807ff0360
marwagalal204 <marwagalal204@gmail.com>
2023-09-28 17:59:21 +0300

    task1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,19 +4,12 @@
  * @n: number
  * Return: binary represntation of a number
 */
-void print_binary_recursive(unsigned long int n)
-{
-	if (n > 1)
-	{
-		print_binary_recursive(n >> 1);
-	}
-	_putchar((n & 1) ? '1' : '0');
-}
 
 void print_binary(unsigned long int n)
 {
-	if (n == 0)
-		putchar('0');
-	else
-		print_binary_recursive(n);
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+	_putchar((n & 1) ? '1' : '0');
 }
