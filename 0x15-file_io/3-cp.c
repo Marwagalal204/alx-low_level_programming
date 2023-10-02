784bb03a6852c1a1e48bc27c12754b16ef641820
marwagalal204 <marwagalal204@gmail.com>
2023-10-03 00:08:17 +0300

    task3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 	r = open(argv[1], O_RDONLY);
-	if (r < 0)
+	if (r == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
@@ -24,25 +24,25 @@ int main(int argc, char *argv[])
 	w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	while ((rd = read(r, buf, 1024)) > 0)
 	{
-		if (w < 0 ||  write(w, buf, rd) != rd)
+		if (w == -1 ||  write(w, buf, rd) != rd)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
 			close(r);
 			exit(99);
 		}
 	}
-	if (rd < 0)
+	if (rd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	close_r = close(r);
 	close_w = close(w);
-	if (close_r < 0 || close_w < 0)
+	if (close_r == -1 || close_w == -1)
 	{
-		if (close_r < 0)
+		if (close_r == -1)
 			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", r);
-		if (close_w < 0)
+		if (close_w == -1)
 			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", w);
 		exit(100);
 	}
