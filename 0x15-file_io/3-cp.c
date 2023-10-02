f0f4d09773a2db76eaa8cdab2bd2876db8262915
marwagalal204 <marwagalal204@gmail.com>
2023-10-02 23:44:35 +0300

    task3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,7 +7,7 @@
 */
 int main(int argc, char **argv)
 {
-	int r, w, rd, wr, close_r, close_w;
+	int r, w, rd, close_r, close_w;
 	char buf[BUFSIZ];
 
 	if (argc != 3)
@@ -21,10 +21,10 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	w = open(argv[2], O_CREAT, O_WRONLY, O_TRUNC, 0664);
+	w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	while ((rd = read(r, buf, BUFSIZ)) > 0)
 	{
-	       	if (w < 0 || (wr = write(w, buf, rd)) != rd)
+	       	if (w < 0 ||  write(w, buf, rd) != rd)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
 			close(r);
@@ -38,6 +38,7 @@ int main(int argc, char **argv)
 	 }
 	close_r = close(r);
 	close_w = close(w);
+	if(close_r < 0 || close_w < 0)
 	{
 		if (close_r < 0)
 			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", r);
