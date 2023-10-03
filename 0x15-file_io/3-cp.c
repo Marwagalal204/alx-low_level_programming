4d09083ff2e585d0dfec74db70384df04bcdb751
marwagalal204 <marwagalal204@gmail.com>
2023-10-03 13:02:35 +0300

    task3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -33,7 +33,7 @@ int main(int argc, char *argv[])
 	}
 	if (rd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]);
 		exit(98);
 	}
 	close_r = close(r);
