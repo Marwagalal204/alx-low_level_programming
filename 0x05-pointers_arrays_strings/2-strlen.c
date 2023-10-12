5cdad441c028427551ecbc62985cef8f3d5d1287
Marwagalal204 <Marwagalal204@gmail.com>
2023-10-12 09:35:01 +0300

    task2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -9,12 +9,8 @@
 
 int _strlen(char *s)
 {
-	int len = 0;
-
-	while (*s != '\0')
-	{
-		s++;
-		len++;
-	}
-	return (len);
+	int i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
 }
--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -16,6 +16,6 @@ void print_array(int *a, int n)
 		if (i != n - 1)
 			printf("%i, ", a[i]);
 		else
-			printf("%i", a[i]);
+			printf("%i\n", a[i]);
 	}
 }
